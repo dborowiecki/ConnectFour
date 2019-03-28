87a4fde1e2fad3ce56a09fe80e5090286e11b2a5
dmn <dborowiecki7@gmail.com>
2019-03-28 20:38:57 +0100

    set up game added

--- src/main/java/GameManager.java
+++ src/main/java/GameManager.java
@@ -1,13 +1,36 @@
-public class GameManager {
+import java.util.Scanner;
 
+public class GameManager {
+    Board b;
+
+    public GameManager(){
+        //setUpGame();
+        //addPlayers();
+    }
     public void startGame(){
 
     }
 
     public void setUpGame(){
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("hello");
+        System.out.print("Default board size? (Y/n)");
+        String def = keyboard.nextLine();
+        if(def.toLowerCase().contains("y"))
+            b = new Board();
+        else {
+            System.out.print("Number of rows: ");
+            int numberOfRows = keyboard.nextInt();
+            System.out.print("Number of columns: ");
+            int numberOfColumns = keyboard.nextInt();
+            b = new Board(numberOfColumns, numberOfRows);
+        }
 
     }
 
+    public void addPlayers(){
+
+    }
     public void addPlayer(){
 
     }
@@ -22,5 +45,9 @@ public class GameManager {
 
     public void updateBoard(){
 
+    }
+
+    public Board getBoard(){
+        return b;
     }
 }
--- src/test/java/GameManagerTest.java
+++ src/test/java/GameManagerTest.java
@@ -2,6 +2,8 @@ import junitparams.JUnitParamsRunner;
 import org.junit.*;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
+
+import java.io.ByteArrayInputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
@@ -15,4 +17,43 @@ import static org.hamcrest.core.Every.everyItem;
 
 @RunWith(JUnitParamsRunner.class)
 public class GameManagerTest {
+
+    GameManager gm;
+
+    @Before
+    public void setUp(){
+        gm = new GameManager();
+    }
+    @After
+    public void teardown(){
+        System.setIn(System.in);
+        gm = null;
+
+    }
+
+    @Test
+    public void setUpGameTestDefaultBoardSize(){
+        ByteArrayInputStream in = new ByteArrayInputStream("Y\n".getBytes());
+        System.setIn(in);
+        gm.setUpGame();
+        System.setIn(System.in);
+        int boardSize = getBoardSize(gm.getBoard());
+        assertThat(boardSize, equalTo(42));
+    }
+
+    @Test
+    public void setUpGameTestDCustomBoardSize(){
+        ByteArrayInputStream in = new ByteArrayInputStream("n\n12\n8".getBytes());
+        System.setIn(in);
+        gm.setUpGame();
+
+        int boardSize = getBoardSize(gm.getBoard());
+        System.setIn(System.in);
+
+        assertThat(boardSize, equalTo(12*8));
+    }
+
+    private int getBoardSize(Board b){
+       return  b.getBoardFields().length*b.getBoardFields()[0].length;
+    }
 }
