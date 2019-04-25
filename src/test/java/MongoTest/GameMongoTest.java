68b1353a53ae5f3220d25ecd9928111925ff81f2
dmn <dborowiecki7@gmail.com>
2019-04-25 19:11:01 +0200

    Mock checkup test add

--- src/test/java/MongoTest/GameMongoTest.java
+++ src/test/java/MongoTest/GameMongoTest.java
@@ -1,28 +1,37 @@
 package MongoTest;
+import Game.TerminalColrs;
 import Mongo.GameCollection;
 import Mongo.GameMongo;
+import Mongo.PlayerMongo;
+import com.mongodb.MongoClient;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.*;
-import org.mockito.Mockito;
 
-import java.util.Arrays;
-import java.util.List;
+
+import static org.mockito.Mockito.doReturn;
 
 @ExtendWith(MockitoExtension.class)
 public class GameMongoTest {
 
     //Co zastepujemy
     @Mock
-    GameCollection friends;
+    GameCollection gameCollection;
 
     //Nasza atrapa
     @InjectMocks
-    GameMongo friendships;
+    GameMongo game;
 
+
+    @Test
+    public void mockingWorskAsExpected(){
+        PlayerMongo joe = new PlayerMongo("Walter White", "red", 0);
+        doReturn(joe).when(gameCollection).findByName("Walter White");
+        Assertions.assertThat(gameCollection.findByName("Walter White")).isEqualTo(joe);
+    }
 
 
 }
