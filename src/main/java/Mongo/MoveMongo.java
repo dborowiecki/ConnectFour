7de0291560141884ce471574284d3515529a392f
dmn <dborowiecki7@gmail.com>
2019-04-25 17:40:45 +0200

    Added move document class

--- src/main/java/Mongo/MoveMongo.java
+++ src/main/java/Mongo/MoveMongo.java
@@ -1,4 +1,29 @@
 package Mongo;
 
 public class MoveMongo {
+    PlayerMongo player;
+    Integer order;
+    Integer column;
+
+    public MoveMongo(PlayerMongo player, int column){
+        setPlayer(player);
+        setColumn(column);
+
+    }
+
+    public void setColumn(Integer column) {
+        this.column = column;
+    }
+
+    public void setPlayer(PlayerMongo player){
+        this.player = player;
+    }
+
+    public void setOrder(Integer order){
+        this.order = order;
+    }
+
+    public MoveMongo getMove(){
+        return this;
+    }
 }
