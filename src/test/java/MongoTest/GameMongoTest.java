63bd1cf8b0c4ecaa41e8ecd83459a3d28fe428c3
dmn <dborowiecki7@gmail.com>
2019-04-25 18:00:52 +0200

    empty test for mockito added

--- src/test/java/MongoTest/GameMongoTest.java
+++ src/test/java/MongoTest/GameMongoTest.java
@@ -1,4 +1,28 @@
 package MongoTest;
+import Mongo.GameCollection;
+import Mongo.GameMongo;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.*;
+import org.mockito.Mockito;
 
+import java.util.Arrays;
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
 public class GameMongoTest {
+
+    //Co zastepujemy
+    @Mock
+    GameCollection friends;
+
+    //Nasza atrapa
+    @InjectMocks
+    GameMongo friendships;
+
+
+
 }
