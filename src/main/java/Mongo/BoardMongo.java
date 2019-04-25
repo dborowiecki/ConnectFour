e1a9e6129c1e0a11bf08886b622b653639606c42
dmn <dborowiecki7@gmail.com>
2019-04-25 17:41:14 +0200

    Added board document class

--- src/main/java/Mongo/BoardMongo.java
+++ src/main/java/Mongo/BoardMongo.java
@@ -1,4 +1,44 @@
 package Mongo;
 
+
 public class BoardMongo {
+    private int rows;
+    private int columns;
+    private int numberOfMoves;
+
+
+    public BoardMongo(int columns, int rows){
+        if(columns<4||rows<4)
+            throw new IllegalArgumentException("Rows and colums need to be bigger than 4, now:\n"
+            +"Rows: "+rows
+            +"\nColumns: "+columns);
+
+        setColumns(columns);
+        setRows(rows);
+        numberOfMoves = 0;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
+
+    public void setColumns(int columns) {
+        this.columns = columns;
+    }
+
+    public void incNumberOfMoves(){
+        numberOfMoves++;
+    }
+    public int getNumberOfMoves(){
+        return numberOfMoves;
+    }
+
 }
